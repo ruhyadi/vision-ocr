9bfd3635366fdb6d9c9dd5d71cc3bc058562102f
Didi Ruhyadi <ruhyadi.dr@gmail.com>
2024-03-28 09:13:18 +0000

    fix: missmatch padding for detection

--- src/engine/ocr_det_onnx_engine.py
+++ src/engine/ocr_det_onnx_engine.py
@@ -27,24 +27,21 @@ class OcrDetOnnxEngine(OnnxEngine):
     def predict(self, img: np.ndarray) -> OcrResultSchema:
         """Detect text from image."""
         img0 = img.copy()
-        img, pad = self.preprocess_img(img)
+        img, pads = self.preprocess_img(img)
         results: List[np.ndarray] = self.engine.run(
             [self.metadata[0].output_name], {self.metadata[0].input_name: img}
         )
-        self._visualize(results)
+        self._visualize_contours(results)
         boxes = self.postprocess_det(
-            results, img0_h=img0.shape[1], img0_w=img0.shape[0], pad=pad
+            results, img0_h=img0.shape[1], img0_w=img0.shape[0], pads=pads
         )
-        for box in boxes:
-            cv2.polylines(img0, [box.astype(np.int32)], True, (0, 255, 0), 2)
-
-        cv2.imwrite("tmp/det.jpg", img0)
+        self._visualize_boxes(img0, boxes)
 
         return results
 
     def preprocess_img(
         self, img: np.ndarray
-    ) -> Tuple[np.ndarray, float, Tuple[int, int]]:
+    ) -> Tuple[np.ndarray, float, Tuple[int, int, int, int]]:
         """Preprocess image for detection model"""
         src_h, src_w = img.shape[:2]
         dst_h = 640 * ((src_h // 640) + 1)
@@ -67,14 +64,14 @@ class OcrDetOnnxEngine(OnnxEngine):
         img = np.transpose(img, (2, 0, 1)) / 255.0  # HWC -> CHW
         img = np.expand_dims(img, axis=0)
 
-        return img.astype(np.float32), (left, top)
+        return img.astype(np.float32), (top, bottom, left, right)
 
     def postprocess_det(
         self,
         results: List[np.ndarray],
         img0_h: int,
         img0_w: int,
-        pad: Tuple[int, int],
+        pads: Tuple[int, int, int, int],
     ) -> OcrResultSchema:
         """Postprocess detection results."""
         # convert batch to single result
@@ -82,10 +79,9 @@ class OcrDetOnnxEngine(OnnxEngine):
         res_h, res_w = result.shape[:2]
 
         # unpad image
-        result = result[
-            int(round(pad[1])) : int(round(pad[1])) + res_h,
-            int(round(pad[0])) : int(round(pad[0])) + res_w,
-        ]
+        # pads in the order of top, bottom, left, right
+        top, bottom, left, right = pads
+        result = result[top : res_h - bottom, left : res_w - right]
 
         # resize image to original size
         result = cv2.resize(result, (img0_h, img0_w))
@@ -116,11 +112,23 @@ class OcrDetOnnxEngine(OnnxEngine):
 
         return boxes
 
-    def _visualize(self, results: List[np.ndarray]) -> None:
+    def _visualize_contours(self, results: List[np.ndarray]) -> None:
         """Visualize detection results."""
         result = results[0].squeeze(0).transpose((1, 2, 0)) * 255.0  # CHW -> HWC
 
-        cv2.imwrite("tmp/det.jpg", result)
+        cv2.imwrite("tmp/det_contours.jpg", result)
+
+        return result
+
+    def _visualize_boxes(self, img: np.ndarray, boxes: List[np.ndarray]) -> None:
+        """Visualize boxes."""
+        img1 = img.copy()
+        for box in boxes:
+            cv2.polylines(img1, [box.astype(np.int32)], True, (0, 255, 0), 2)
+
+        cv2.imwrite("tmp/det_boxes.jpg", img)
+
+        return img1
 
 
 if __name__ == "__main__":
